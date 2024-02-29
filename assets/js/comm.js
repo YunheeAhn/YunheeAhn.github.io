45080860f7c2338548b5da11ecdbeb841e3dfc5e
YunheeAhn <ayh95944674@gmail.com>
2024-02-29 14:58:06 +0900

    240229_Remove swiper.js

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -106,39 +106,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let ScrollTrigger = gsap.to(".turn-sPlanet",{x:"-100px", duration: 1.5});
 
-    // 스와이퍼
-    var swiper = new Swiper(".mySwiper", {
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-            },
-        });
-});
 
-// $(document).ready(function () {
-//     // fullpage
-//     $("#fullpage").fullpage({
-//       responsiveWidth: 480,
-//     });
-//     $("#fullpage").fullpage({
-//       scrollingSpeed: 700,
-//       // scrollBar: true,
-//       onLeave: function (origin, destination, direction) {
-//         // 빠른전환으로 이벤트중복시 fullpage와 swiper전환시점 분리막기
-//         $("#fullpage").on("scroll touchmove mousewheel", function (event) {
-//           event.preventDefault();
-//           event.stopPropagation();
-//           return false;
-//         });
-//         swiper.mousewheel.disable();
-//       },
-//       afterLoad: function (anchorLink, index) {
-//         // 전환이 끝난후 이벤트풀기
-//         $("#fullpage").off("scroll mousewheel");
-//         if (!$(".fp-completely .swiper-wrapper").length > 0)
-//           $("#fullpage").off("touchmove"); // 모바일분기
-//         if (swiper) swiper.mousewheel.enable();
-//         if (!$(".sec3").hasClass("active")) $.fn.fullpage.setAllowScrolling(true); // 슬라이드 섹션을 벗어나면 휠풀어주기
-//       },
-//     });
-// })
+});
